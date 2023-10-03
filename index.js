341da395be0f393995be616e2d3490a55e464e8c
Ish Kattur <ikattur@opentechalliance.com>
2023-10-03 14:01:51 -0700

    rename Reports folder for Org use

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ async function getMemberActivity(orgid, from, to, contribArray) {
     })
 
     // Prepare path/filename, repo/org context and commit name/email variables
-    const reportPath = `reports/${org}-${new Date().toISOString().substring(0, 19).replaceAll(':', '.') + 'Z'}-${fileDate}.csv`
+    const reportPath = `Reports/${org}-${new Date().toISOString().substring(0, 19).replaceAll(':', '.') + 'Z'}-${fileDate}.csv`
     const committerName = core.getInput('committer-name', {required: false}) || 'github-actions[bot]'
     const committerEmail = core.getInput('committer-email', {required: false}) || '41898282+github-actions[bot]@users.noreply.github.com'
     const {owner, repo} = github.context.repo
